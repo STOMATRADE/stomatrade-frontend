e443ed84913db88fbba8a2fe40f1b42e51f0d82f
Wahid <decode.std@gmail.com>
2025-12-22 21:51:05 +0700

    add new cpde

--- src/app/(admin)/dashboard/page.tsx
+++ src/app/(admin)/dashboard/page.tsx
@@ -107,17 +107,17 @@ export default function DashboardPage() {
     const totalInvestmentAmount = investmentsData?.data?.reduce((sum, inv) => sum + inv.amount, 0) || 0;
 
     return (
-        <main className="w-full max-w-[1440px] mx-auto">
+        <main className="w-full max-w-360 mx-auto">
             <section className="flex flex-col items-start pt-10 sm:pt-12 md:pt-14 pb-8 sm:pb-10 md:pb-12">
                 <div className="bg-[#4ade8026] border border-[#4ade80] rounded-2xl px-4 py-2 mb-4 sm:mb-6 md:mb-[16px]">
                     <span className="text-xs sm:text-sm md:text-xs font-semibold leading-lg text-[#b4b4b4]">
                         Admin
                     </span>
                 </div>
-                <h1 className="text-[28px] sm:text-[35px] md:text-[50px] font-medium leading-[28px] sm:leading-[35px] md:leading-[50px] text-text-primary mb-3 sm:mb-4 md:mb-[12px]">
+                <h1 className="text-[28px] sm:text-[35px] md:text-[50px] font-medium leading-[28px] sm:leading-8.75 md:leading-12.5 text-text-primary mb-3 sm:mb-4 md:mb-[12px]">
                     Dashboard Overview
                 </h1>
-                <p className="text-base sm:text-lg md:text-2xl font-normal leading-[20px] sm:leading-[22px] md:leading-[25px] text-text-placeholder max-w-2xl">
+                <p className="text-base sm:text-lg md:text-2xl font-normal leading-5 sm:leading-[22px] md:leading-6.25 text-text-placeholder max-w-2xl">
                     Ringkasan singkat aktivitas platform untuk admin.
                 </p>
             </section>
